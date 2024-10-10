080c505a1ba786a21918dc9b17d2eadb71e14599
Hyeona01 <sha0209@dgu.ac.kr>
2024-10-10 22:12:45 +0900

    fix: 줄맞춤 center 통일

--- src/components/home/services/HomeProject.tsx
+++ src/components/home/services/HomeProject.tsx
@@ -25,7 +25,7 @@ const TextSection = () => (
 const HomeProject = () => {
   return (
     <section className="flex items-center justify-center bg-yellow_50 p-card_padding">
-      <div className="w-[1440px] flex items-center justify-start py-[100px]">
+      <div className="w-[1440px] flex items-center justify-center py-[100px]">
         <div className="flex items-center gap-[50px]">
           <Image
             src={GiggleCard}
